ca82f3ba506a08b82163db64e5c65a22da42f2f8
Daniel <danieldodson10@gmail.com>
2018-03-12 12:59:06 -0400

    Initial Commit

---
+++ Project 1 attempt 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 1 attempt 4
+//
+//  Created by Daniel Dodson on 3/12/18.
+//  Copyright © 2018 Daniel Dodson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
